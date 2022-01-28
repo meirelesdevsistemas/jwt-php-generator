cd7fd7466c799bf5cc9126d486dd71c259523a7e
Daniel Meireles <daniel@inovadora.com.br>
2022-01-28 11:56:29 -0300

    finished tests of class.

--- tests/Entity/JwtPHPGeneratorTest.php
+++ tests/Entity/JwtPHPGeneratorTest.php
@@ -9,16 +9,44 @@ use PHPUnit\Framework\TestCase;
 
 class JwtPHPGeneratorTest extends TestCase
 {
-    public function testShouldCreateAJWTString()
+    protected $instance;
+    protected $data;
+
+    protected function setUp(): void
     {
-        $data = [
+        $this->instance = new JwtPHPGenerator('secret_key');
+        $this->data = [
             "id" => 1,
             "name" => 'Tester',
             "email" => 'tester@testes.com.br'
         ];
-        $jwtGenerator = new JwtPHPGenerator('secret_key');
+    }
+    public function testShouldCreateAJWTString()
+    {
+        $data = $this->data;
+        $jwtGenerator = $this->instance;
         $token = $jwtGenerator->create($data);
 
         $this->assertIsString($token);
+    }
+    public function testValidateToken()
+    {
+        $data = $this->data;
+        $jwtGenerator = $this->instance;
+        $token = $jwtGenerator->create($data);
+        $response = $jwtGenerator->validate($token);
+        $this->assertEquals($this->data, $response);
+    }
+    public function testIsAnInvalidToken()
+    {
+        $data = [
+            "id"=>2,
+            "name"=>"Tester",
+            "email" =>"tester@testes.com.br"
+        ];
+        $jwtGenerator = $this->instance;
+        $token = $jwtGenerator->create($this->data);
+        $response = $jwtGenerator->validate($token);
+        $this->assertNotEquals($data, $response);
     }
 }
